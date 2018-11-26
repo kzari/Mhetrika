2c1a139705ae4f5a121b723576bc4e05a073dace
Ronye do Lago <ronye.rocha@gmail.com>
2018-11-26 16:41:50 -0200

    first

---
+++ mhetrika.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mhetrika.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ mhetrika.core/Entities/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mhetrika.core.Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
