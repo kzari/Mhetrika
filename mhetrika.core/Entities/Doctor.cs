2dcdf07e7d2dfbc377e2fe639e363e58dfd7d9ba
Ronye do Lago <ronye.rocha@gmail.com>
2018-11-27 10:20:25 -0200

    doctor

---
+++ mhetrika.core/Entities/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mhetrika.core.Entities
+{
+    class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Crm { get; set; }
+        public string Phone { get; set; }
+        public string Specialty { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
