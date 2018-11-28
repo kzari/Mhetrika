3c8272407057803d3bd96ac0150073f071831446
Ronye do Lago <ronye.rocha@gmail.com>
2018-11-28 15:36:40 -0200

    patient

--- mhetrika.core/Entities/User.cs
+++ mhetrika.core/Entities/User.cs
@@ -7,6 +7,8 @@ namespace mhetrika.core.Entities
     public class User
     {
         public int Id { get; set; }
+        public virtual Patient Patient { get; set; }
+        public int PatientId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
