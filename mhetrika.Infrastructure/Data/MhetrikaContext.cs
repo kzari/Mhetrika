d9360ef20bb54df43aa36b936d2181b334ea8dbf
Ronye do Lago <ronye.rocha@gmail.com>
2018-11-28 16:55:45 -0200

    context

---
+++ mhetrika.Infrastructure/Data/MhetrikaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mhetrika.Infrastructure.Data
+{
+    public class MhetrikaContext : DbContext
+    {
+        public MhetrikaContext(DbContextOptions<MhetrikaContext> options) : base(options)
+        {
+
+        }
+    }
+}
