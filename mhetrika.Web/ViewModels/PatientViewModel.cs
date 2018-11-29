65a8f12f76f04642d99b374855000b50215955cb
Ronye do Lago <ronye.rocha@gmail.com>
2018-11-29 13:48:37 -0200

    ViewModels

---
+++ mhetrika.Web/ViewModels/PatientViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mhetrika.Web.ViewModels
+{
+    public class PatientViewModel
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+    }
+}
