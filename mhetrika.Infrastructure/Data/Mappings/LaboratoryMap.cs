a4b0cdf00fdde2aee9da5248503210f8f18187dd
Ronye do Lago <ronye.rocha@gmail.com>
2018-12-03 19:42:34 -0200

    exam map

---
+++ mhetrika.Infrastructure/Data/Mappings/LaboratoryMap.cs
@@ -0,0 +1,14 @@
+﻿using mhetrika.core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace mhetrika.Infrastructure.Data.Mappings
+{
+    public class LaboratoryMap : IEntityTypeConfiguration<Laboratory>
+    {
+        public void Configure(EntityTypeBuilder<Laboratory> builder)
+        {
+            
+        }
+    }
+}
